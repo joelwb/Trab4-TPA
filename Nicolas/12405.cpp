91758a230100e3bdfeadf5e0bff64c6035f0c2c9
NicolasSampaio <nicolas.pereira.sampaio16@gmail.com>
2019-12-05 12:37:32 -0300

    accepted

---
+++ Nicolas/12405.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*O problema avalia a quantidade de espantalhos que deve ser colocado na colheita para garantir que irá espantar todas as ameaças*/
+
+	int qntCase;
+	cin >> qntCase;
+	for (int turno = 0; turno < qntCase; ++turno) {
+		int qntCaracteres;
+		cin >> qntCaracteres;
+		string campo;
+		cin >> campo;
+		int qntEspantalhos = 0;
+		for (int posCampo = 0; posCampo < campo.size(); posCampo++) {
+			if (campo[posCampo] == '.') {
+				qntEspantalhos++;
+				if (posCampo + 2 > campo.size()) {
+					break;
+				}
+				posCampo = posCampo + 2;
+			}
+		}
+		int nCase = turno + 1;
+		cout << "Case ";
+		cout << nCase;
+		cout << ": ";
+		cout << qntEspantalhos << endl;
+	}
+}
