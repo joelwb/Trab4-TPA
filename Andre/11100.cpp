e2a8209a480c2cedb0e3587147be1e454f09b639
André Martins <objetovazio@gmail.com>
2019-12-06 02:43:37 -0300

    Update 11100.cpp
    finalizado

--- Andre/11100.cpp
+++ Andre/11100.cpp
@@ -4,47 +4,60 @@
 
 using namespace std;
 
+const int max_knapsack = 10000;
 
 int main()
 {
-	int totalKnapsack, knapsacksSize[10001];
+	int totalKnapsack, knapsacksSize[max_knapsack];
 
-		// Primeiramente lê o total de knapsacks dados aos viajantes
-		while ((cin >> totalKnapsack) && totalKnapsack)
-		{
-			// Lê a tamanho de cada knapsack
-			for (int i = 0; i < totalKnapsack; ++i) cin >> knapsacksSize[i];
+	// Primeiramente lê o total de knapsacks dados aos viajantes
+	cin >> totalKnapsack;
+	while (totalKnapsack)
+	{
+		// Lê a tamanho de cada knapsack
+		for (int i = 0; i < totalKnapsack; ++i) cin >> knapsacksSize[i];
 			
-			// Ordena o array de tamanhos
-			sort(knapsacksSize, knapsacksSize + totalKnapsack);
+		// Ordena o array de tamanhos
+		sort(knapsacksSize, knapsacksSize + totalKnapsack);
 
-			// Faz um loop por todos tamanhos e pega a maior quantidade de mochilas do mesmo tamanho
-			int count_repeticao = 1, n_maior_repeticao = 0;
-			knapsacksSize[totalKnapsack] = 0; // Garante que o último caso será verificado 
-			for (int i = 0; i < totalKnapsack; i++) {
-				if (knapsacksSize[i] == knapsacksSize[i + 1]) count_repeticao++;
-				else {
-					n_maior_repeticao = count_repeticao > n_maior_repeticao 
-						? count_repeticao 
-						: n_maior_repeticao;
+		// Faz um loop por todos tamanhos e pega a maior quantidade de mochilas do mesmo tamanho
+		int count_repeticao = 1, n_maior_repeticao = 0;
+		knapsacksSize[totalKnapsack] = 0; // Garante que o último caso será verificado 
+		for (int i = 0; i < totalKnapsack; i++) {
 
-					count_repeticao = 1;
-				}
-			}
+			// Verifica se elemento i se repete em i + 1
+			if (knapsacksSize[i] == knapsacksSize[i + 1]) count_repeticao++;
+			else {
+				// Mantém guardado o maior valor de repetições
+				n_maior_repeticao = count_repeticao > n_maior_repeticao 
+					? count_repeticao 
+					: n_maior_repeticao;
 
-			// Printa o maior valor de repetições 
-			cout << n_maior_repeticao << "\n";
-
-			// Primeiro loop que passa por todo array de tamanhos
-			for (int i = 0; i < n_maior_repeticao; ++i) {
-				
-				// Segundo loop para passa
-				for (int j = i; j < totalKnapsack; j += n_maior_repeticao) {
-					if (i != j) cout << " ";
-					cout << knapsacksSize[j];
-				}
-				
-				cout << "\n";
+				// reseta o contador
+				count_repeticao = 1;
 			}
 		}
+
+		// Printa o maior valor de repetições 
+		cout << n_maior_repeticao << "\n";
+		
+		// Primeiro loop para percorrer cada elemento que se repete
+		int i = 0;
+		while (i < n_maior_repeticao) {
+			// Segundo loop para definir quais knapsack ficaram guardados em determiado elemento
+			for (int j = i; j < totalKnapsack; j += n_maior_repeticao) {
+				if (i != j) cout << " ";
+				cout << knapsacksSize[j];
+			}
+			cout << '\n';
+
+			i++;
+		}
+
+		// Verificac se é o fim
+		cin >> totalKnapsack;
+		if (totalKnapsack) {
+			cout << '\n';
+		}
+	}
 }
